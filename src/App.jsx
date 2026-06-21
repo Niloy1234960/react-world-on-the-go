40528a27be7db5443077177d6bd31a2858d8b6ee
Niloy Mondal <niloymondal1370@gmail.com>
2026-06-21 12:41:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+          <Countries></Countries>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
